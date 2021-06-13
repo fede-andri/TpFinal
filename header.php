dcc54f7f3272e8c243baf4b12878cb4e05657915
fede-andri <fede_andrijasevich@hotmail.com>
2021-06-13 20:28:22 -0300

    Inicio de proyecto, se agrego  header, footer, los roles y reportes

---
+++ header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/TpFinal/css/w3.css">
+    <link rel="stylesheet" href="/TpFinal/css/headerFooterStyle.css">
+    <title>Document</title>
+</head>
+<body>
+    <header id="header">
+        <img src="#" alt="Logo">
+
+        <div id="inicio-sesion">
+            <p>Bienvenido Admi</p>
+            <a href="#" id="cerrar-sesion">Cerrar Sesion</a>
+        </div>
+    </header>
+    <nav id="navegador" class="w3-center">
+        <div>
+            <a href="/TPFinal/reportes.php">REPORTES</a>
+            <a href="/TpFinal/roles.php">ROLES</a>
+            <a href="#">VEHICULOS</a>
+        </div>
+    </nav>
+</body>
+</html>
