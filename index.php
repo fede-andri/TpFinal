71d3d34951521eb12264ba6327914eeac4967a66
Andrijasevich Federico <fede_andrijasevich@hotmail.com>
2021-06-19 01:10:58 -0300

    Se arreglaron nombre en rutas

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                     <label for="clave">Contraseña:</label>
                     <input type="password" id="clave" name="clave" class="w3-input w3-margin-bottom" aria-required="true" aria-invalid="true">
 
-                    <a id="btnLogin" class="w3-btn-block w3-margin-top" href="/TpFinal/view/reportes.php">INGRESAR</a>
+                    <a id="btnLogin" class="w3-btn-block w3-margin-top" href="/TpFinal/view/reportesView.php">INGRESAR</a>
                     <a id="btnOlvideClave" class="w3-transparent w3-block w3-margin-top w3-center w3-text-blue" href="">Olvidé mi contraseña</a>
 
                 </form>
